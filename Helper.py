15d9b4ba519420950c34af52ceb30ff3a0b31703
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-10-03 20:13:37 +0200

    add example

--- Helper.py
+++ Helper.py
@@ -1,4 +1,4 @@
-user_input = input("type a number:"34.5")
+user_input = input() # Нехай користувач надрукував 4
 print(type(user_input)) # <class 'str'>
 number = int(user_input)
 print(number / 2) # Виведе 2
