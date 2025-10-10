362cd71895a6023df54187566233e4373312ff88
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-10-10 17:34:11 +0200

    lesson_3 hometask 3.2

--- hw_3.2.py
+++ hw_3.2.py
@@ -1,13 +1,8 @@
 my_list = [1,2,3,4,5,6,7,8,9]
-a = my_list.pop(-1)
-my_list.insert(0,a)
-print(my_list)
-
-
-my_list = [1,2,3,4,5,6,7,8,9]
-a = my_list[-1]
-my_list.remove(a)
-my_list.insert(0,a)
-print(my_list)
-
+if len(my_list)>1:
+    a = my_list.pop(-1)
+    my_list.insert(0,a)
+    print(my_list)
+else:
+    print(my_list)
 
