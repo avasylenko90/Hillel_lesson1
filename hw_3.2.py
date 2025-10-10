05e6a6c94fdce09cd42e640445c8b08c06da196a
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-10-10 17:22:20 +0200

    lesson_3 hometask 3.2

---
+++ hw_3.2.py
@@ -0,0 +1,13 @@
+my_list = [1,2,3,4,5,6,7,8,9]
+a = my_list.pop(-1)
+my_list.insert(0,a)
+print(my_list)
+
+
+my_list = [1,2,3,4,5,6,7,8,9]
+a = my_list[-1]
+my_list.remove(a)
+my_list.insert(0,a)
+print(my_list)
+
+
