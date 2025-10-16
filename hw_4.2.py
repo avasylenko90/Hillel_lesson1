bc9ecb02ef6610e83e8db8f54ecfe96859b20ee5
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-10-16 15:58:56 +0200

    lesson_4 hometask 4.2 - fixed indentation

--- hw_4.2.py
+++ hw_4.2.py
@@ -1,10 +1,8 @@
 my_list = [1, 3, 5]
 if my_list:
-        new_list = my_list[::2]
-        total = sum(new_list)
-        final_list = total* my_list[-1]
-        print(final_list)
+    new_list = my_list[::2]
+    total = sum(new_list)
+    final_list = total * my_list[-1]
+    print(final_list)
 else:
-        print("0")
-
-
+    print("0")
