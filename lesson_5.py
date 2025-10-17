e114048982c33b3e4cc093c897cdc4b5473a7e8e
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-10-17 12:56:35 +0200

    lesson_5 hometask 5.1

--- lesson_5.py
+++ lesson_5.py
@@ -1,8 +1,11 @@
-x = input("Please create the password: ")
+x = input("Please create the name of variable: ")
 y = list(x)
 if y[0].isdigit():
-    print("Password can't start with digit")
+    print("The name of variable can't start with the digit")
 for i in y:
     if i.isupper():
-        print("Password can't start with uppercase")
-
+        print("The name of variable can't contain uppercase letters")
+        break
+    if i in """!"#$%&'()*+,-./:;<=>?@[\]^`{|}~""":
+        print("The name of variable can't contain punctuation")
+        break
