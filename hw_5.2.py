a0da24f9ea3ee44047925c3f757b7da99bcb50d9
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-10-17 17:47:51 +0200

    lesson_5 hometask 5.2

--- hw_5.2.py
+++ hw_5.2.py
@@ -19,38 +19,3 @@ while check == "yes":
         print("❌ Error: division by zero!")
 
     check = input("Would you like to continue (yes / no)?")
-
-# while d == "y":
-#     d = input("Would you like to continue (y/n)? ")
-#     a = input("enter first number: ")
-#     b = int(input("enter second number: "))
-#     c = input("enter arithmetic operator (+, -, *, /): ")
-#     if c == "+":
-#         print(a + b)
-#     elif c == "-":
-#         print(a - b)
-#     elif c == "*":
-#         print(a * b)
-#     elif c == "/":
-#         if b != 0:
-#             print(a / b)
-#         else:
-#             print("❌ Error: division by zero!")
-# print("End")
-#
-# print("Please one by one enter floating digits and math action identifying what to do with them: ")
-# a = float(input("enter first number: "))
-# b = float(input("enter second number: "))
-# c = input("enter arithmetic operator (+, -, *, /): ")
-# if c == "+":
-#     print(a + b)
-# elif c == "-":
-#     print(a - b)
-# elif c == "*":
-#     print(a * b)
-# elif c == "/":
-#     if b != 0:
-#         print(a / b)
-#     else:
-#         print("❌ Error: division by zero!")
-# print("the end")
