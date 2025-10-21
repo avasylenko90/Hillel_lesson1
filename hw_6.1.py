60a80d56f38339d9c3f2cd0ed9f6543a4307b9e3
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-10-21 23:33:33 +0200

    lesson_6 hometask_6_1

--- hw_6.1.py
+++ hw_6.1.py
@@ -1,7 +1,7 @@
 import string
 user_input = input("Enter 2 letter via '-' : ")
-user_input2 = user_input.split("-")
-x = string.ascii_letters.index(user_input2[0])
-y = string.ascii_letters.index(user_input2[1])
+split_input = user_input.split("-")
+x = string.ascii_letters.index(split_input[0])
+y = string.ascii_letters.index(split_input[1])
 z = string.ascii_letters[x : y + 1]
 print(z)
