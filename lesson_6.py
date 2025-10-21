32dc70d412b30c8f52754b8141767405f58970fc
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-10-22 00:04:42 +0200

    lesson_6

--- lesson_6.py
+++ lesson_6.py
@@ -1,5 +1,13 @@
-range(5)
-print(range(5))
-r = range(5)
-for i in range(5):
-    print(i)
+import random
+import string
+
+user_input = input("Enter password length: ")
+input_int = int(user_input)
+
+new_str = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+for _ in range(input_int):
+    password += random.choice(new_str)
+
+print(password)
