006db2b2a4cacf6c0b2aebad545b8dd163940dee
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-10-24 19:23:04 +0200

    lesson_8 homework 8

--- lesson_8.py
+++ lesson_8.py
@@ -2,4 +2,4 @@ def fruitset(**kwargs):
     for fruit, price in kwargs.items():
         print(f"{fruit} costs {price}")
 
-fruitset(aplles=10, banana=20, cherry=40, orange=50)
+fruitset(aplles=10, banana=20, cherry=40, orange=50, pineapple=60)
