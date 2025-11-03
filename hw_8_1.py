ca555f1b7ea8e13c396899b637c7e06cd30174af
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-11-03 20:43:47 +0100

    lesson_8 homework 8.1

--- hw_8_1.py
+++ hw_8_1.py
@@ -1,10 +1,11 @@
-def add_one(random_list):
+def add_one(some_digits):
      list = []
-     interim = int(random_list)
+     interim = int(some_digits)
      list.append(interim)
-     new_list = (list[0]+1)
-     print(new_list)
+     new_digit = (list[0] + 1)
+     final_list = [int(x) for x in str(new_digit)]
+     print(final_list)
 
 
-random_list = input("please enter any number of random digits:")
-add_one(random_list)
+some_digits = input("please enter any number of random digits:")
+add_one(some_digits)
