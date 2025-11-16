e84bcf6bad101bc6f1adf31f729afa7ded9a5e34
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-11-16 19:39:46 +0100

    lesson_11 homework 11.1

---
+++ hw/lesson_11/hw_11_1.py
@@ -0,0 +1,20 @@
+def prime_generator(end):
+    if n < 2:
+        return False
+    for i in range(2, n + 1):
+        if n % i == 0:
+            break
+    return i == n
+
+def prime_generator(end):
+    for num in range(2, end + 1):
+        if IsPrime(num):
+            yield num
+from inspect import isgenerator
+
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
