edd79713b861bd8ead7022a6806a89a5cdc26bc4
Alina Vasylenko <alinavasylenko1990@gmail.com>
2025-11-18 17:05:50 +0100

    revised 11.1 task

--- hw/lesson_11/hw_11_1.py
+++ hw/lesson_11/hw_11_1.py
@@ -16,3 +16,4 @@ assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
 assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
 assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
 print('Ok')
+
